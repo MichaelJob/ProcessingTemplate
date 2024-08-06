575ac3540dea0fe0edbfb3c3826bd16921d75e3c
Michael Job <mj@michaeljob.ch>
2024-08-06 19:41:24 +0200

    added QlockTwoStarterApp for ws2C

--- src/main/java/ip12/MainClass.java
+++ src/main/java/ip12/MainClass.java
@@ -2,6 +2,7 @@ package ip12;
 
 import ip12.DemoApp.DemoApp;
 import ip12.Pong.PongApp;
+import ip12.ws2cQlockTwo.QlockTwoStarterApp;
 import processing.core.PApplet;
 
 public class MainClass extends PApplet {
@@ -9,6 +10,7 @@ public class MainClass extends PApplet {
         //define which ProcessingApp should run on start (also import it)
 
         //PApplet.main(DemoApp.class, args);
-        PApplet.main(PongApp.class, args);
+        //PApplet.main(PongApp.class, args);
+        PApplet.main(QlockTwoStarterApp.class, args);
     }
 }
